a9247d5666018e0a54bb1d4463b0bd007178aeda
Brad Donohue <brad@braddonohue.com>
2021-11-15 23:10:49 +0000

    Add master page template

---
+++ resources/views/page.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title') | COPYandPAY</title>
+</head>
+<body>
+  <header>
+    <h1>COPYandPAY</h1>
+    <hr>
+    <br>
+  </header>
+  <main>
+    @yield('content')
+  </main>
+</body>
+</html>
