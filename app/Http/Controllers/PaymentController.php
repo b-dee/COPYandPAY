e7a9d55cc79638954d0aebd23f42d818f5cecb67
Brad Donohue <brad@braddonohue.com>
2021-11-25 20:35:05 +0000

    WIP refactor of PaymentController, general cleanup

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,11 +6,11 @@ use App\Models\Payment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 class PaymentController extends Controller
 {
-    const PAYMENT_ENDPOINT_BASE = 'https://eu-test.oppwa.com/';
-    const SUCCESS_RESULT_CODE = '000.100.110'; // Integrator Test Mode success
+    const REGEX_PREPARE_SUCCESS = '/^(000\.200)/';
 
     public function index($id = null)
     {
@@ -20,7 +20,8 @@ class PaymentController extends Controller
         ]);
     }
 
-    public function history() {
+    public function history() 
+    {
         // In a production app I'd be paginating this properly, 
         // but we're only going to have a few records here... 
         $payments = Payment::where('user_id', Auth::id())->get();
@@ -30,100 +31,115 @@ class PaymentController extends Controller
         ]);
     }
 
+    protected static function makePrepareBody(Payment $payment, string $entityId): array 
+    {
+        return [
+            'entityId' => $entityId,
+            'merchantTransactionId' => $payment->merchant_tx_id,
+            'amount' => $payment->amount,
+            'currency' => $payment->currency,
+            'paymentType' => $payment->type,
+            'paymentBrand' => $payment->brand
+        ];
+    }
+
+    protected static function validatePrepareResponse($response): bool 
+    {
+        $code = $response['result']['code'] ?? null;
+        return $code !== null && preg_match(self::REGEX_PREPARE_SUCCESS, $code) === 1;
+    }
+
     public function pay(Request $request)
     {
-        // Just some simple validation, not too realistic
-        $validatedDetails = $request->validate([
-            'amount' => ['required', 'min:0.01', 'max:999999', 'numeric'],
-            'reference' => ['required', 'min:8', 'max:50']
+        $valid = $request->validate([
+            'amount' => [
+                'required', 
+                'numeric', 
+                'min:0.01', 
+                'max:9999999999.99'
+            ],
+            'reference' => [
+                'required', 
+                'string', 
+                'min:8', 
+                'max:255', 
+                'unique:App\Models\Payment,merchant_tx_id'
+            ]
         ]);
 
-        $url = self::PAYMENT_ENDPOINT_BASE . 'v1/checkouts';
-        $entityId = env('COPY_AND_PAY_ENTITY_ID');
-        $accessToken = env('COPY_AND_PAY_ACCESS_TOKEN');
-        $merchantTxId = $validatedDetails['reference'];
-        $amount = strval($validatedDetails['amount']);
+        $payment = new Payment();
+        $payment->user_id = Auth::id();
+        $payment->merchant_tx_id = $valid['reference'];
+        $payment->amount = $valid['amount'];
+        $payment->currency = 'GBP';
+        $payment->type = 'DB';
+        $payment->brand = 'VISA';
 
-        // Hardcode these for now
-        // TODO: Allow merchant to set these
-        $currency = 'GBP';
-        $paymentType = 'DB';
+        $prepareUrl = Config::get('copyandpay.base_url') . 'v1/checkouts';
+        $entityId = Config::get('copyandpay.entity_id');
+        $accessToken = Config::get('copyandpay.access_token');
 
-        $data = "entityId={$entityId}" .
-            "&merchantTransactionId={$merchantTxId}" .
-            "&amount={$amount}" .
-            "&currency={$currency}" .
-            "&paymentType={$paymentType}";
-        
-        
-        $response = Http::withToken($accessToken)
-            ->withBody($data, 'application/x-www-form-urlencoded')
-            ->post($url);
+        $response = Http::asForm()
+            ->withToken($accessToken)
+            ->post($prepareUrl, self::makePrepareBody($payment, $entityId))
+            ->throw()
+            ->json();
 
-        if ($response->ok()) {
-            $res = $response->json();
-            if (is_array($res) && isset($res['id'])) {
-                $checkoutId = $res['id'];
-
-                // Store merchant tx reference for use later
-                $payment = new Payment();
-                $payment->user_id = Auth::id();
-                $payment->merchant_tx_id = $merchantTxId;
-                $payment->amount = $amount;
-                $payment->currency = $currency;
-                $payment->save();
-
-                return redirect("/pay/{$checkoutId}");
-            }
+        if (!self::validatePrepareResponse($response)) {
+            return abort(500, 'Could not prepare checkout');
         }
+        
+        // Save the pending payment
+        $payment->save();
 
-        // TODO: Proper error handling and display in a real app
-        // I won't implement here since this is a demo app
-        return 'oops!';
+        return redirect("/pay/{$response['id']}");
+    }
+
+    protected static function validateResultResponse($response): bool 
+    {
+        $code = $response['result']['code'] ?? null;
+        return $code !== null && preg_match(self::REGEX_PREPARE_SUCCESS, $code) === 1;
     }
 
     public function result(Request $request)
     {
         $resourcePath = $request->query('resourcePath');
 
-        if ($resourcePath !== null) {
-            $url = self::PAYMENT_ENDPOINT_BASE . $resourcePath;
-            $entityId = env('COPY_AND_PAY_ENTITY_ID');
-            $accessToken = env('COPY_AND_PAY_ACCESS_TOKEN');
-
-            $response = Http::withToken($accessToken)->get($url, [
-                'entityId' => $entityId
-            ]);
-
-            if ($response->ok()) {
-                $res = $response->json();
-                if (is_array($res)) {
-                    $resultCode = $res['result']['code'];
-                    $resultDesc = $res['result']['description'];
-                    $merchantTxId = $res['merchantTransactionId'];
-
-                    // Update payment record with result
-                    $payment = Payment::where('merchant_tx_id', $merchantTxId)->update([
-                        'result_code' => $resultCode,
-                        'result_desc' => $resultDesc
-                    ]);
-
-                    $resultMsg = $resultCode === self::SUCCESS_RESULT_CODE
-                        ? 'Successful'
-                        : 'Failed';
-                    
-                    return view('result', [
-                        'resultMsg' => $resultMsg,
-                        'resultCode' => $resultCode,
-                        'resultDesc' => $resultDesc
-                    ]);
-                }
-            }
-
+        if ($resourcePath === null) {
+            return abort(500, 'Could not get checkout result');
         }
 
-        // TODO: Proper error handling and display in a real app
-        // I won't implement here since this is a demo app
-        return 'oops!';
+        $resultUrl = Config::get('copyandpay.base_url') . $resourcePath;
+        $entityId = Config::get('copyandpay.entity_id');
+        $accessToken = Config::get('copyandpay.access_token');
+
+        $response = Http::withToken($accessToken)
+            ->get($resultUrl, ['entityId' => $entityId])
+            ->throw()
+            ->json();
+
+        if (!self::validateResultResponse($response)) {
+            return abort(500, 'Could not process payment');
+        }
+
+        $resultCode = $response['result']['code'];
+        $resultDesc = $response['result']['description'];
+        $merchantTxId = $response['merchantTransactionId'];
+
+        // Update payment record with result
+        $payment = Payment::where('merchant_tx_id', $merchantTxId)->update([
+            'result_code' => $resultCode,
+            'result_desc' => $resultDesc
+        ]);
+
+        $resultMsg = $resultCode === self::SUCCESS_RESULT_CODE
+            ? 'Successful'
+            : 'Failed';
+        
+        return view('result', [
+            'resultMsg' => $resultMsg,
+            'resultCode' => $resultCode,
+            'resultDesc' => $resultDesc
+        ]);
     }
 }
