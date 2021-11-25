b8cd586e920ca371774037968f9ecdf5c7f86db1
Brad Donohue <brad@braddonohue.com>
2021-11-25 20:33:45 +0000

    Move to using config facade over env helper directly

---
+++ config/copyandpay.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'base_url' => env('COPY_AND_PAY_BASE_URL', 'https://eu-test.oppwa.com/'),
+  'entity_id' => env('COPY_AND_PAY_ENTITY_ID'),
+  'access_token' => env('COPY_AND_PAY_ACCESS_TOKEN')
+];
